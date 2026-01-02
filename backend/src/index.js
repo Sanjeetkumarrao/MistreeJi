5082b637292af0646f5c4d1fc1fc7831586fc80e
Sanjeetkumarrao <sanjeetkumarbharti2018@gmail.com>
2026-01-02 21:20:19 +0530

    added database connection files

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 import dotenv from "dotenv"
 import express from "express"
+import connectDB from "./db/index.js";
 dotenv.config({
     path: './.env'
-})
+});
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,6 +16,13 @@ app.get("/sk",(req, res) => {
     res.send(`Server is running at Sk`);
 });
 
-app.listen(PORT,() => {
+
+connectDB()
+.then(() => {
+    app.listen(PORT,() => {
     console.log(`Server running at PORT : http://localhost:${PORT}`)
+});
+})
+.catch((error) => {
+    console.log("MongoDB connection Failed !!! ",error);
 });
